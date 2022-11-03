1634c5be910dd3c0b028fb1c6889527f080d4ad6
jreppiks <31517228+jreppiks@users.noreply.github.com>
2022-11-03 14:27:01 -0600

    Update lplookup.sh
    'mo bettah update from @hateshape

--- lplookup.sh
+++ lplookup.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-### https://www.youtube.com/watch?v=YZKP12tYofo ###
-usage() {
-	echo "Usage: $0 [-p PLATE NUMBER] [-s 2 CHAR STATE ABBREVIATION]" 
-	exit 1
-}
+USERAGENT='User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.63 Safari/537.36'
+ACCENCODING='Accept-Encoding: gzip, deflate'
+ACCLANG='Accept-Language: en-US,en;q=0.9'
 
-while getopts "p:s:" flag
-do
-	case "${flag}" in
-		p) 
-			plate=${OPTARG}
-			;;
-		s) 
-			state=${OPTARG}
-			;;
-		*) 
-			usage;;
-	esac
+usage() {
+  echo "Usage: $0 [-p PLATE NUMBER] [-s 2 CHAR STATE ABBREVIATION]"
+  exit 1
+}
+while getopts "p:s:" flag; do
+  case "${flag}" in
+  p|plate)
+    plate=${OPTARG}
+    ;;
+  s|state)
+    state=${OPTARG}
+    ;;
+  *)
+    usage
+    ;;
+  esac
 done
 
-if [ -z "$plate" ] || [ -z "$state" ]; then
-	echo 'missing option' >&2
-	usage
-fi
+[[ -z "${plate-}" ]] && echo "Missing required parameter: p|plate" && exit
+[[ -z "${state-}" ]] && echo "Missing required parameter: s|state" && exit
 
-curl -i -s -k -X $'GET' \
-   -H $'Host: www.oreillyauto.com' -H $'Sec-Ch-Ua: \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"' -H $'Sec-Ch-Ua-Mobile: ?0' -H $'Sec-Ch-Ua-Platform: \"Linux\"' -H $'Upgrade-Insecure-Requests: 1' -H $'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.63 Safari/537.36' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H $'Sec-Fetch-Site: none' -H $'Sec-Fetch-Mode: navigate' -H $'Sec-Fetch-User: ?1' -H $'Sec-Fetch-Dest: document' -H $'Accept-Encoding: gzip, deflate' -H $'Accept-Language: en-US,en;q=0.9' -H $'Connection: close' "https://www.oreillyauto.com/vehicle/plate/$plate/$state/true" | grep plate | python -m json.tool
+curl -s -k -H "$USERAGENT" -H "$ACCENCODING" -H "$ACCLANG" "https://www.oreillyauto.com/vehicle/plate/$plate/$state/true" | jq
